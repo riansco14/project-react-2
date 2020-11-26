222efae84d3470f58ace90a7a8a767a93161eff5
riansco14 <rian.rabelo@ufrpe.br>
2020-11-26 20:47:23 -0300

    First Commit

---
+++ front-end/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Login from './pages/Login'
+import GlobalStyle from './styles/global'
+
+const App: React.FC = () => {
+	return (
+		<>
+			<Login />
+			<GlobalStyle />
+		</>
+	)
+}
+
+export default App
