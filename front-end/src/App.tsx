f4b4e1189bbe6b89798d436332c5843bcb895b94
riansco14 <rian.rabelo@ufrpe.br>
2020-11-26 21:21:50 -0300

    Tela Cadastro

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import Login from './pages/Login'
 import GlobalStyle from './styles/global'
-
+import { BrowserRouter, Route, Router, Switch } from 'react-router-dom'
+import Cadastro from './pages/Cadastro'
 const App: React.FC = () => {
 	return (
 		<>
-			<Login />
+			<BrowserRouter>
+				<Switch>
+					<Route path="/login">
+						<Login />
+					</Route>
+					<Route path="/cadastro">
+						<Cadastro />
+					</Route>
+				</Switch>
+			</BrowserRouter>
 			<GlobalStyle />
 		</>
 	)
--- front-end/src/pages/Cadastro/index.tsx
+++ front-end/src/pages/Cadastro/index.tsx
@@ -1,24 +1,32 @@
 import React from 'react'
 import { Background, Container, Content } from './styles'
 import logoImg from '../../assets/logo.svg'
-import { FiLock, FiLogIn, FiMail } from 'react-icons/fi'
+import { FiArrowLeft, FiLock, FiLogIn, FiMail, FiUser } from 'react-icons/fi'
 import Input from '../../components/Input'
 import Button from '../../components/Button'
 import { Link } from 'react-router-dom'
 
-const Login: React.FC = () => (
+const Cadastro: React.FC = () => (
 	<Container>
+		<Background />
 		<Content>
 			<img src={logoImg} alt="Logo" />
 			<form action="POST">
-				<h1>Faça seu logon</h1>
+				<h1>Faça seu cadastro</h1>
+				<Input
+					icon={FiUser}
+					type="text"
+					name="name"
+					placeholder="Nome"
+					id=""
+				/>
 				<Input
 					icon={FiMail}
 					type="text"
 					name="email"
 					placeholder="E-mail"
 					id=""
-				></Input>
+				/>
 				<Input
 					icon={FiLock}
 					type="password"
@@ -26,17 +34,15 @@ const Login: React.FC = () => (
 					placeholder="Senha"
 					id=""
 				/>
-				<Button type="submit">Entrar</Button>
-				<a href="forgot">Esqueci minha senha</a>
+				<Button type="submit">Cadastrar</Button>
 			</form>
 
-			<Link to="/cadastro">
-				<FiLogIn />
-				Criar conta
+			<Link to="/login">
+				<FiArrowLeft />
+				Voltar para logon
 			</Link>
 		</Content>
-		<Background />
 	</Container>
 )
 
-export default Login
+export default Cadastro
